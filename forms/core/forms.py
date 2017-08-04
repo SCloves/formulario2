cb035bd66a190de8310b6bd08d7b41815650ea0e
Cloves Adriano <cloves.sousa@prognoos.com>
2017-08-04 14:09:50 -0300

    ...

--- forms/core/forms.py
+++ forms/core/forms.py
@@ -1,1 +1,9 @@
-from forms import models
+from django import forms
+
+from .models import Formulario
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Formulario
+        fields = ('nome', 'email', 'telefone')
