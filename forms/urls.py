f2ff3b0687774753f108e977f24cc37e813f18d4
Cloves Adriano <cloves.sousa@prognoos.com>
2017-08-04 14:54:22 -0300

    ...

--- forms/urls.py
+++ forms/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from forms.core.views import formulario
 
 urlpatterns = [
-	url(r'^$', 'forms.core.views.formulario'),
+	url(r'^$', formulario),
     url(r'^admin/', include(admin.site.urls)),
 ]
