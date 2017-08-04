5d79b7625a0c536fcbaf8a72327e5c39c427675a
Cloves Adriano <cloves.sousa@prognoos.com>
2017-08-04 15:14:50 -0300

    ...

--- forms/core/views.py
+++ forms/core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .forms import PostForm
 
 # Create your views here.
 
 def formulario(requests):
-	return render(requests, 'core/layout.html')
+	form = PostForm()
+	return render(requests, 'core/layout.html', {'form': form})
