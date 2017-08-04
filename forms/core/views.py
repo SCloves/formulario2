030ba8b6e9a20402485a9b7393ab2d0612c9eaf0
Cloves Adriano <cloves.sousa@prognoos.com>
2017-08-04 18:23:33 -0300

    ...

--- forms/core/views.py
+++ forms/core/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import PostForm
 
 # Create your views here.
 
-def formulario(requests):
-	form = PostForm()
-	return render(requests, 'core/layout.html', {'form': form})
+def formulario(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.nome = request.POST['nome']
+            post.email = request.POST['email']
+            post.telefone = request.POST['telefone']
+            post.save()
+            return redirect('/')
+    else:
+        form = PostForm()
+    return render(request, 'core/layout.html', {'form': form})
